e3bdc954b106ff0c625ab6a4197164ddf450c0e1
illex <illex@pop-os.localdomain>
2020-11-09 13:23:34 -0700

    added buttons and button handlers for filtering in timeline

---
+++ js/timeline.js
@@ -0,0 +1,40 @@
+class Timeline{
+
+    constructor(){
+        //initialize all values
+    }
+
+    draw(){
+        //put all the buttons where they go
+    }
+}
+
+let button1Handler = function(){
+    //TODO: fill this in
+    console.log("implement This")
+    console.log("button1Handler");
+}
+
+let changeData = function(){
+    //TODO: fill this in
+    console.log("implement This")
+    console.log("changing data");
+}
+
+let changeOrder = function(){
+    //TODO: fill this in
+    console.log("implement This")
+    console.log("change order")
+}
+
+let selectAllHandler = function(){
+    //TODO: fill this in
+    console.log("implement This")
+    console.log("selectAll")
+}
+
+let clearHandler = function(){
+    //TODO: fill this in
+    console.log("implement This")
+    console.log("clear")
+}
