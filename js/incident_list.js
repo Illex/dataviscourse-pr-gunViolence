5019bc5195c5312f6ceaaf006ad206082dc3e89b
illex <illex@pop-os.localdomain>
2020-11-11 12:45:58 -0700

    fixed processing issue in timeline, added labels, text, mouseove, and outline to story list

--- js/incident_list.js
+++ js/incident_list.js
@@ -4,15 +4,95 @@ class IncidentList{
         this.data = d;
 
         //add event listener
-        document.getElementById("scroll").addEventListener("input", printValue);
     }
 
     draw(){
-        //add svg and g element
-        //map rectangles
-        //map text labels
-        //draw rectangl
-        //TODO: fill this in
+        document.getElementById("scroll").addEventListener("input", printValue);
+
+        let selection = d3.select(".incident_list").append("svg").attr("id", "incident-box-container")
+            .attr("width", "280") 
+            .attr("height", "500") 
+            .attr("transform", "translate(-10, -260)"); 
+            
+            //use the selection to append a group of rectangles who's y position are changed based on the slider position
+
+        let eventList = d3.select("#incident-box-container").append("g").attr("id", "incident-list");
+        let rects = eventList.selectAll("rect").data(this.data[0]);
+
+        //create the rectangles for each event
+        let that = this;
+        rects.join(
+            enter =>
+                enter
+                .append("rect")
+                .attr("x", "10")
+                //.attr("y", "10")
+                .attr("y", function(d, i){
+                    return i * 45 ;
+                })
+                .attr("width", "260")
+                .attr("height", "40")
+                .attr("opacity", ".6")
+                .style("fill", "steelBlue")
+                .on("mouseover", function(d){
+                    d3.select(this).attr("opacity", "1")
+                })
+                .on("mouseout", function(d){
+                    d3.select(this).attr("opacity", ".6")
+                })
+        )
+
+        //create the text labels for each rect
+        eventList = d3.select("#incident-box-container").append("g").attr("id", "incident-list-text");
+        let labels = eventList.selectAll("text").data(this.data[0])
+        labels.join(
+            enter =>
+                enter
+                .append("text")
+                .attr("font-family", "sans-serif")
+                .attr("font-size", "16")
+                .attr("transform", function(d, i){
+                    return "translate(20," + (i * 45 + 30) + ")";
+                })
+                .text(function(d){
+                    return d.date + " id: " + d.id;
+                })
+        )
+
+        let outline = d3.select("#incident-box-container").append("g").attr("id", "incident-box-left");
+        //left 
+        outline.append("line")
+            .attr("x1", "5")
+            .attr("x2", "5")
+            .attr("y1", "0")
+            .attr("y2", "500")
+            .attr("stroke", "black")
+            .attr("stroke-width", "4");
+        //right 
+        //outline = d3.select("#incident-box-container").append("g").attr("id", "incident-box-right");
+        outline.append("line")
+            .attr("x1", "275")
+            .attr("x2", "275")
+            .attr("y1", "0")
+            .attr("y2", "500")
+            .attr("stroke", "black")
+            .attr("stroke-width", "4");
+        //top
+        outline.append("line")
+            .attr("x1", "5")
+            .attr("x2", "275")
+            .attr("y1", "2")
+            .attr("y2", "2")
+            .attr("stroke", "black")
+            .attr("stroke-width", "4");
+        //bottom
+        outline.append("line")
+            .attr("x1", "5")
+            .attr("x2", "275")
+            .attr("y1", "498")
+            .attr("y2", "498")
+            .attr("stroke", "black")
+            .attr("stroke-width", "4");
     }
 
     update(){
@@ -22,5 +102,5 @@ class IncidentList{
 
 let printValue = function(){
     let temp = document.getElementById("scroll").value
-    console.log(temp);
+    //console.log(temp);
 }
--- js/timeline.js
+++ js/timeline.js
@@ -21,49 +21,51 @@ class Timeline{
         //get gun violence to population rate 
 
         //storage for the rate at each month
-        //let globalRate = []
+        let globalRate = []
         //the default date
-        //let date = "2013-01";
-        //let counter = 0
+        let date = "2013-01";
+        let counter = 0
 
         //for every event
-        //for(let i = 0; i < this.data[0].length; i++){
+        for(let i = 0; i < this.data[0].length; i++){
             //if it's date is equal to the current one, increment
-         //   if(this.data[0][i].date.substr(0,7) == this.date){
-          //      counter++;
-           // }
+            if(this.data[0][i].date.substr(0,7) === date){
+                counter++;
+            }
             //if it's not, we're done counting events in that month
-            //else{
-          //      globalRate.push(counter);
-           //     this.date = this.data[0][i].date;
-            //    counter = 0;
-            //}
-        //}
-        //console.log(globalRate);
+            else{
+                //console.log(this.data[0][i].date.substr(0,7))
+                globalRate.push(counter);
+                date = this.data[0][i].date.substr(0,7);
+                counter = 0;
+            }
+        }
+        console.log("global rate");
+        console.log(globalRate);
 
         //add up the total population for each year
-//        let population = []
- //       populaiton["2013"] = 0;
-  //      populaiton["2014"] = 0;
-   //     populaiton["2015"] = 0;
-    //    populaiton["2016"] = 0;
-     //   populaiton["2017"] = 0;
-      //  populaiton["2018"] = 0;
+        let population = []
+        population["2013"] = 0;
+        population["2014"] = 0;
+        population["2015"] = 0;
+        population["2016"] = 0;
+        population["2017"] = 0;
+        population["2018"] = 0;
 
-       // for(let i = 0; i < this.data[1].length; i++)
-        //    populaiton["2013"] = this.data[1][i].pop2013;
-         //   populaiton["2014"] = this.data[1][i].pop2014;
-          //  populaiton["2015"] = this.data[1][i].pop2015;
-           // populaiton["2016"] = this.data[1][i].pop2016;
-            //populaiton["2017"] = this.data[1][i].pop2017;
-            //populaiton["2018"] = this.data[1][i].pop2018;
-        //}
+        for(let i = 0; i < this.data[1].length; i++){
+            population["2013"] = this.data[1][i].pop2013;
+            population["2014"] = this.data[1][i].pop2014;
+            population["2015"] = this.data[1][i].pop2015;
+            population["2016"] = this.data[1][i].pop2016;
+            population["2017"] = this.data[1][i].pop2017;
+            population["2018"] = this.data[1][i].pop2018;
+        }
 
         //console.log("population")
         //console.log(population)
 
         console.log(this.data)
-        let selection = d3.select(".timeline").append("svg").attr("height", 410).attr("width", 610).attr("id", "time-line")
+        //let selection = d3.select(".timeline").append("svg").attr("height", 410).attr("width", 610).attr("id", "time-line")
         //append g for each element on the svg
 
         //x axis goes from 0 to 63
