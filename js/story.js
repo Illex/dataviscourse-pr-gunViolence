1f1b4cc9c5992add3adfc8e0b174a54bcf6ccad9
illex <illex@pop-os.localdomain>
2020-11-12 11:50:39 -0700

    story pane outline rework

--- js/story.js
+++ js/story.js
@@ -5,65 +5,48 @@ class Story{
     }
 
     draw(){
-
-        //upper outline
-            d3.select(".story")
-            .append("svg").attr("id", "outline-top")
-            .attr("width", "300")
-            .attr("height", "20")
-            d3.select("#outline-top").append("g").append("line")
-            .attr("x1", "0")
-            .attr("x2", "300")
-            .attr("y1", "6")
-            .attr("y2", "6")
-            .attr("stroke", "black")
-            .attr("opacity", ".8")
-            .attr("stroke-width","4");
-
-        //bottom outline
-            d3.select(".story")
-            .append("svg").attr("id", "outline-bottom")
-            .attr("transform", "translate(0,472)")
-            .attr("width", "300")
-            .attr("height", "20")
-            d3.select("#outline-bottom").append("g").append("line")
-            .attr("x1", "0")
-            .attr("x2", "300")
-            .attr("y1", "6")
-            .attr("y2", "6")
-            .attr("stroke", "black")
-            .attr("opacity", ".8")
-            .attr("stroke-width","4");
-        
-        //left outline
-            d3.select(".story")
-            .append("svg").attr("id", "outline-left")
-            .attr("transform", "translate(0,-43)")
-            .attr("width", "4")
-            .attr("height", "500")
-            d3.select("#outline-left").append("g").append("line")
-            .attr("x1", "2")
-            .attr("x2", "2")
-            .attr("y1", "0")
-            .attr("y2", "498")
-            .attr("stroke", "black")
-            .attr("opacity", ".8")
-            .attr("stroke-width","4");
-
-        //right outline
-            d3.select(".story")
-            .append("svg").attr("id", "outline-right")
-            .attr("transform", "translate(292,-43)")
-            .attr("width", "4")
-            .attr("height", "500")
-            d3.select("#outline-right").append("g").append("line")
-            .attr("x1", "2")
-            .attr("x2", "2")
-            .attr("y1", "0")
-            .attr("y2", "498")
-            .attr("stroke", "black")
-            .attr("opacity", ".8")
-            .attr("stroke-width","4");
+         let eventPane = d3.select(".story").append("svg").attr("id", "incident-pane")
+                .attr("width", "280")
+                .attr("height", "500")
+                .attr("transform", "translate(16, 4)")
+                //.attr("transform", "translate(-10, -260)");
+         let outline = d3.select("#incident-pane").append("g").attr("id", "incident-pane-outline");
+         //left 
+         outline.append("line")
+             .attr("x1", "5")
+             .attr("x2", "5")
+             .attr("y1", "0")
+             .attr("y2", "500")
+             .attr("stroke", "black")
+             .attr("opacity", ".8")
+             .attr("stroke-width", "4");
+         //right 
+         outline.append("line")
+             .attr("x1", "275")
+             .attr("x2", "275")
+             .attr("y1", "0")
+             .attr("y2", "500")
+             .attr("stroke", "black")
+             .attr("opacity", ".8")
+             .attr("stroke-width", "4");
+         //top
+         outline.append("line")
+             .attr("x1", "5")
+             .attr("x2", "275")
+             .attr("y1", "2")
+             .attr("y2", "2")
+             .attr("stroke", "black")
+             .attr("opacity", ".8")
+             .attr("stroke-width", "4");
+         //bottom
+         outline.append("line")
+             .attr("x1", "5")
+             .attr("x2", "275")
+             .attr("y1", "498")
+             .attr("y2", "498")
+             .attr("stroke", "black")
+             .attr("opacity", ".8")
+             .attr("stroke-width", "4");
     }
 
     update(){
