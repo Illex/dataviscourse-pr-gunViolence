1d108bee600f27f891219deb5527c9ebfeb9a9ec
alexandra-hurst <alexandra.hurst@utah.edu>
2020-11-26 21:32:59 -0700

    stuff

--- js/by_gender.js
+++ js/by_gender.js
@@ -1,22 +1,31 @@
 class Gender{
 
+    /*Split by injury/death
+    largest attack by state
+    */
     constructor(data, year, states){
         this.data = data[2];
+        this.dist = data[3];
         this.year = year;
         this.width = 300;
-        this.height = 4900;
+        this.height = 490;
         this.states = states;
+
+        this.scaleX = d3.scaleLinear().domain([0,10]).range([0, this.width])
     }
 
-    addState(state){
+    addState(states){
         this.states = states;
-        this.filteredData = this.data.filter(d => d.state in states)
+        this.filteredData = this.data.filter(d => this.states.contains(d.state))
     }
 
     drawHist(){
+        let that = this;
         let svg = d3.select('.by_gender').append('svg').attr('height', this.height)
-            .attr('width', this.width).attr('transform', 'translate(5,5)')
+            .attr('width', this.width).append('g').attr('transform', 'translate(5,5)')
+        svg.append("g").attr("transform", "translate(0," + this.height + ")")
+            .call(d3.axisBottom(this.scaleX))
 
 
-    }
+
 }
--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ words = []
 words[0] = d3.json("data/data.txt")
 words[1] = d3.json("data/stateData.txt")
 words[2] = d3.json("data/stateData.json")
+words[3] = d3.json('data/count_distribution.json')
 
 //data at 0 is the gun violence json
 //data at 1 is the state population
