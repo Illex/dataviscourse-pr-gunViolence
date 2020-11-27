7d34e80892b9c2f99a50f2ae06fd30baa886a9ac
alexandra-hurst <alexandra.hurst@utah.edu>
2020-11-27 10:20:00 -0700

    changes

--- js/by_gender.js
+++ js/by_gender.js
@@ -10,8 +10,8 @@ class Gender{
         this.width = 300;
         this.height = 490;
         this.states = states;
+        this.scaleX = d3.scaleLinear().domain([0,10]).range([0, this.width]);
 
-        this.scaleX = d3.scaleLinear().domain([0,10]).range([0, this.width])
     }
 
     addState(states){
@@ -19,13 +19,48 @@ class Gender{
         this.filteredData = this.data.filter(d => this.states.contains(d.state))
     }
 
-    drawHist(){
+    drawHist() {
         let that = this;
         let svg = d3.select('.by_gender').append('svg').attr('height', this.height)
             .attr('width', this.width).append('g').attr('transform', 'translate(5,5)')
         svg.append("g").attr("transform", "translate(0," + this.height + ")")
-            .call(d3.axisBottom(this.scaleX))
 
+        let maxes = [];
+        for(let s in this.dist[this.year]){
+            let max = 0;
+            let val = {};
+            let data = this.dist[this.year][s]
+            for(let i = 0; i < data.length; i++){
+                let d = data[i];
+                if(d['deaths'] > max){
+                    max = d['deaths'];
+                    d['state'] = s;
+                    val = d;
+                }
+            }
+            maxes.push(val);
+        }
+        let colorScale = d3.scaleSequential().interpolator(d3.interpolateBlues)
+            .domain([d3.min(maxes.map(d => d.deaths)), d3.max(maxes.map(d => d.deaths))]);
+        let size = d3.scaleLinear().domain([0, d3.max(maxes.map(d => d.deaths))])
+            .range([7,50])
+        let Tooltip = d3.select(".by_gender")
+            .append("div")
+            .style("opacity", 0)
+            .attr("class", "tooltip")
+            .style("background-color", "white")
+            .style("border", "solid")
+            .style("border-width", "2px")
+            .style("border-radius", "3px")
+            .style("padding", "5px")
 
-
+        let mouseover = function(d) {
+            Tooltip
+                .style("opacity", 1)
+        }
+        let mouseleave = function(d) {
+            Tooltip
+                .style("opacity", 0)
+        }
+    }
 }
